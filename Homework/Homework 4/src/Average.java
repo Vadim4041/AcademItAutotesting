02eb9e44a0d52dc488d014fd2ad132a32a3b17b9
Vadim4041 <kozhevnikov404@yandex.ru>
2022-08-17 19:12:09 +0700

    updated Average.java

--- Homework/Homework 4/src/Average.java
+++ Homework/Homework 4/src/Average.java
@@ -4,23 +4,25 @@ public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Введите первое число:");
         int start = scanner.nextInt();
+
+        System.out.println("Введите второе число:");
         int end = scanner.nextInt();
 
-        int difference = end - start;
         int sum = 0;
         int sumEven = 0;
         int amountOfEvenNumbers = 0;
 
         for (int i = start; i <= end; i++) {
-            sum += (start + i);
-            if ((start + i) % 2 == 0) {
-                sumEven += (start + i);
+            sum +=  i;
+            if (i % 2 == 0) {
+                sumEven += i;
                 amountOfEvenNumbers += 1;
             }
         }
 
-        double average = (double) sum / (difference + 1);
+        double average = (double) sum / ((end - start) + 1);
         double averageEven = (double) sumEven / amountOfEvenNumbers;
 
         System.out.printf("Среднее арифметическое всех чисел диапазона от %d до %d включительно равняется %.3f.", start, end, average);
