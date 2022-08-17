18452ba74f256d43cac5f989a1b7fabe1ebc7090
Vadim4041 <kozhevnikov404@yandex.ru>
2022-08-17 11:16:00 +0700

    added Digits.java

---
+++ Homework/Homework 4/src/Digits.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Digits {
+    public static void main(String[] args) {
+        System.out.println("Введите число:");
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        int sumDigits = 0;
+        int sumOddDigits = 0;
+        int maxDigit = 0;
+        while (a != 0) {
+            int digit = a % 10;
+            if (maxDigit < digit) {
+                maxDigit = digit;
+            }
+            if (IsOdd(digit)) {
+                sumOddDigits += digit;
+            }
+            sumDigits += digit;
+            a = a / 10;
+        }
+        System.out.println("Максимальная цифра числа: " + maxDigit);
+        System.out.println("Сумма цифр числа: " + sumDigits);
+        System.out.println("Сумма нечетных цифр числа: " + sumOddDigits);
+    }
+
+    public static boolean IsOdd(int x) {
+        return x % 2 == 1;
+    }
+
+}
