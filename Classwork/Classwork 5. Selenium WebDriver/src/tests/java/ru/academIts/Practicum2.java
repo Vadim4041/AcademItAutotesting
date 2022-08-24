f54310f9d6beccd35dbd896856a6a7dc315f0bce
Vadim4041 <kozhevnikov404@yandex.ru>
2022-08-24 17:37:46 +0700

    updated Practicum2 in classwork 5

--- Classwork/Classwork 5. Selenium WebDriver/src/tests/java/ru/academIts/Practicum2.java
+++ Classwork/Classwork 5. Selenium WebDriver/src/tests/java/ru/academIts/Practicum2.java
@@ -3,7 +3,6 @@ package ru.academIts;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -12,13 +11,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
-import static org.openqa.selenium.By.cssSelector;
 
 public class Practicum2 {
     private WebDriver driver;
@@ -46,7 +40,6 @@ public class Practicum2 {
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        ;
     }
 
     @AfterEach
